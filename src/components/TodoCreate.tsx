4680aec221320ec173ad779fdd43c0eecda0eaae
CanerYesiltas <caneryesiltas1@gmail.com>
2025-01-10 22:02:21 -0500

    feat:add redux slices and store

--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 const TodoCreate = () => {
   return (
     <div className='todo-create'>
-      <input className='todo-input' type='text' placeholder='Enter Your Todo' />
+      <input
+        className='todo-input'
+        type='text'
+        placeholder='Enter your space mission 🛰️🚀'
+      />
       <button className='todo-create-button'> Create </button>
     </div>
   );
