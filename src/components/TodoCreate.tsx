a3cac54533e9b3e4d820bffb19ea3b9e082875ca
CanerYesiltas <caneryesiltas1@gmail.com>
2025-01-11 02:28:02 -0500

    feat: add handlecreatetodo function & action creator

--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import type { AppDispatch } from '../redux/store';
+
+import { TodoType } from '../types/Types';
+import { createTodo } from '../redux/todoSlice';
+import { useDispatch } from 'react-redux';
 
 const TodoCreate = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  const handleCreateTodo = () => {
+    if (newTodo.trim().length == 0) {
+      alert('Enter Your Missinon');
+      return;
+    }
+
+    const payload: TodoType = {
+      id: Math.floor(Math.random() * 999999),
+      content: newTodo,
+    };
+
+    dispatch(createTodo(payload));
+    setNewTodo('');
+  };
+
   return (
     <div className='todo-create'>
       <input
+        value={newTodo}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setNewTodo(e.target.value)
+        }
         className='todo-input'
         type='text'
         placeholder='Enter your space mission 🛰️🚀'
       />
-      <button className='todo-create-button'> Create </button>
+      <button onClick={handleCreateTodo} className='todo-create-button'>
+        {' '}
+        Create{' '}
+      </button>
     </div>
   );
 };
