aa1e02233eba2197ae546d4a12540800a38bab4e
CanerYesiltas <caneryesiltas1@gmail.com>
2025-01-12 00:19:28 -0500

    Add: Update function for todos

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdRemoveCircleOutline } from 'react-icons/io';
 import { FaCheck } from 'react-icons/fa';
 import { FaRegEdit } from 'react-icons/fa';
 import { TodoType } from '../types/Types';
 import { useDispatch } from 'react-redux';
-import {removeTodoById} from '../redux/todoSlice'
+import { removeTodoById, updateTodo } from '../redux/todoSlice';
 interface TodoProps {
   todoProps: TodoType;
 }
 
 const Todo = ({ todoProps }: TodoProps) => {
   const { id, content } = todoProps;
-
   const dispatch = useDispatch();
 
-  const handleRemoveTodo= () => {
-dispatch(removeTodoById(id))
-  }
+  const [editable, setEditable] = useState<boolean>(false);
+  const [newTodo, setNewTodo] = useState<string>(content);
+
+  const handleRemoveTodo = () => {
+    dispatch(removeTodoById(id));
+  };
+
+  const handleUpdateTodo = () => {
+    const payload: TodoType = {
+      id: id,
+      content: newTodo,
+    };
+    dispatch(updateTodo(payload));
+    setEditable(false);
+  };
 
   return (
     <div
@@ -29,16 +40,37 @@ dispatch(removeTodoById(id))
         padding: '1rem',
         marginTop: '1.5625rem',
         borderRadius: '.3125rem',
-        color:'black'
+        color: 'black',
       }}
     >
-      <div>{content}</div>
+      {editable ? (
+        <input
+          type='text'
+          style={{
+            width: '400px',
+            border: 'none',
+            borderBottom: '.0625rem solid lightgrey',
+            outline: 'none',
+          }}
+          value={newTodo}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setNewTodo(e.target.value)
+          }
+        />
+      ) : (
+        <div> {content} </div>
+      )}
       <div>
-        <IoMdRemoveCircleOutline onClick={handleRemoveTodo}
+        <IoMdRemoveCircleOutline
+          onClick={handleRemoveTodo}
           className='icons'
           style={{ marginRight: '.5rem' }}
         />
-        <FaRegEdit className='icons'  />
+        {editable ? (
+          <FaCheck onClick={handleUpdateTodo} />
+        ) : (
+          <FaRegEdit onClick={() => setEditable(true)} className='icons' />
+        )}
       </div>
     </div>
   );
